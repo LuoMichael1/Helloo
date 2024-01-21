ef636b970e9acbcbd40803b783ecf68aa6e3093b
LuoMichael1 <136616479+LuoMichael1@users.noreply.github.com>
2024-01-20 23:48:35 -0500

    responsive image grid
    the renders page's grids now resize based on the width of the window courtesy of js

---
+++ script.js
@@ -0,0 +1,53 @@
+let window_width = window.innerWidth;
+let tile_width;
+
+function hello() {
+    document.getElementById("test").style.color = "red";
+    //getWidth();
+}
+
+function getWidth() {
+    console.log(window_width);
+
+    new ResizeObserver(() => {
+        window_width = window.innerWidth;
+        changeWidth();
+    }).observe(document.body)
+
+    
+}
+
+
+
+function changeWidth() {
+    
+
+
+    tile_width = window_width*0.8;
+
+    if (tile_width > 700) {
+        tile_width /= 3;
+        tile_width -= 11;
+        document.getElementById("renders").style.gridTemplateColumns = tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px';
+        document.getElementById("renders").style.gridTemplateAreas = "'" + "a b c' " + "'" + "d e e' " + "'" + "f e e' " + "'" + "f g h' " + "'" + "i j k' " + "'" + "l m k' " + "'" + "n o p' ";
+        'a b c'
+		'd e e'
+		'f e e'
+		'f g h'
+		'i j k'
+		'l m k'
+		'n o p';
+    }
+    else {
+        tile_width /= 2;
+        //tile_width -=;
+        document.getElementById("renders").style.gridTemplateColumns = tile_width + 'px ' + tile_width + 'px';
+        document.getElementById("renders").style.gridTemplateAreas = "'" + "a b' " + "'" + "c d' " + "'" + "e e' " + "'" + "e e' " + "'" + "f g' " + "'" + "f h' " + "'" + "i k' " + "'" + "j k' " + "'" + "l m' " + "'" + "n o' ";
+    }
+    
+    
+    document.getElementById("renders").style.gridTemplateRows = tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px ' + tile_width + 'px';
+
+    //: 1fr 1fr 1fr;
+	//grid-template-rows: 460px 460px 460px;
+}
