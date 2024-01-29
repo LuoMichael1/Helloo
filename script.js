26d8a1508e62a790eb286c243384549306f7d9d5
LuoMichael1 <136616479+LuoMichael1@users.noreply.github.com>
2024-01-29 15:40:22 -0500

    improvements
    - cleaned up the keyframes in css
    - made the website a bit better on mobile with media queries
    - changed the color of the butterflies
    - added dates to the pictures on the picture home page

--- script.js
+++ script.js
@@ -40,10 +40,10 @@ function setButterFly(butterID) {
     let size = 30 + Math.random()*100;
     let frame = Math.floor(Math.random()*7);
 
-    document.getElementById("butterflys").innerHTML += '<img class="butterfly" id="butterfly'+butterID+'" alt="" src="images/butterfly'+frame+'.gif">';
+    document.getElementById("butterflys").innerHTML += '<img draggable="false" class="butterfly" id="butterfly'+butterID+'" style="--order: '+butterID+'" alt="" src="images/butterfly'+frame+'.gif">';
     document.getElementById("butterfly" + butterID).style.width = ''+size+'px';
     document.getElementById("butterfly" + butterID).style.rotate = 60+frame+'deg';
-    document.getElementById("butterfly" + butterID).style.animation = 'flap'+butterID+' 566.66ms linear infinite, move 1'+time+'s linear infinite -'+frame+"s";
+    document.getElementById("butterfly" + butterID).style.animation = 'flap'+' 566.66ms linear infinite, move 1'+time+'s linear infinite -'+frame+"s";
 }
 
 
